b7a4c1f921762b952f9721e9aaa86888b05da3ec
Amy Michelle <amy.azogue@fazogue.local>
2023-09-20 23:21:58 -0400

    fixed indentation error

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,14 +1,14 @@
 class ResaleShop:
 
     # What attributes will it need?
-"""
+    """
 storing the inventory for the store
 updating a computer's price
 updating a computer's OS
 buying a computer (add to inventory)
 selling a computer (remove from inventory)
 refurbishing a computer
-"""
+    """
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
     def __init__(self, inventory: int, itemID: int ):
