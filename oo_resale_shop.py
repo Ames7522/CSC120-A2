807cfe02a54175210c3f7ee2b56e6386aac46be8
Amy Michelle <amy.azogue@fazogue.local>
2023-09-20 23:04:10 -0400

    making the basic set up for Resale Shop and starting the rubric

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,20 @@
 class ResaleShop:
 
     # What attributes will it need?
-
+"""
+storing the inventory for the store
+updating a computer's price
+updating a computer's OS
+buying a computer (add to inventory)
+selling a computer (remove from inventory)
+refurbishing a computer
+"""
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self, inventory: int, itemID: int ):
+        self.inventory = inventory
+        self.itemID = itemID
+        
+        #pass # You'll remove this when you fill out your constructor
 
     # What methods will you need?
